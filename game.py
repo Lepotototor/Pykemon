3f8c95fb77b4c3f7f4e82d93d9b99e09b179daf1
gubgub63 <enzogubbiotti63@gmail.com>
2022-11-23 21:47:02 +0100

    nouveaux menu

--- game.py
+++ game.py
@@ -1,10 +1,13 @@
-import pygame
+import pygame , sys 
 import pytmx
 import pyscroll
+from button import Button
 
 from joueur import Joueur
 from map import Map
 
+def get_font(size): # Returns Press-Start-2P in the desired size
+            return pygame.font.Font("assets/font.ttf", size)
 
 
 class Jeux:
@@ -92,7 +95,50 @@ class Jeux:
         for sprite in self.map.calques.sprites():
             if sprite.bas_du_joueur.collidelist(self.map.collisions) > -1:
                 sprite.retour_arrriere()
+                
+    def main_menu():
+        SCREEN = pygame.display.set_mode((1280, 720))
+        pygame.display.set_caption("Menu")
 
+        BG = pygame.image.load("assets/Background.png")
+        
+        
+        while True:
+            SCREEN.blit(BG, (0, 0))
+
+            MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+            MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+            MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+            PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250), 
+                                text_input="PLAY",  font=get_font(75),base_color="#d7fcd4", hovering_color="White")
+            OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(640, 400), 
+                                text_input="OPTIONS", font=get_font(75),base_color="#d7fcd4", hovering_color="White")
+            QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550), 
+                                text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+            SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+            for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+                button.changeColor(MENU_MOUSE_POS)
+                button.update(SCREEN)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                        jeux = Jeux()
+                        jeux.run()
+                    if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                        options()
+                    if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                        pygame.quit()
+                        sys.exit()
+
+            pygame.display.update()
 
     def run(self):
 
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from game import Jeux
 if __name__ == "__main__":
     pygame.init()
     jeux = Jeux()
-    jeux.run()
+    Jeux.main_menu()
